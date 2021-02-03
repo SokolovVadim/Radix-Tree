abefa210097955cf6665174095c8dc7b44ebd097
Vadim <varem99@mail.ru>
2021-02-03 23:35:34 +0300

    generate string

---
+++ radix_test.go
@@ -0,0 +1,42 @@
+package radix
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestRadixInterface(t *testing.T) {
+	r := New()
+	r.Insert("foo", 1)
+	r.Insert("bar", 2)
+	r.Insert("foobar", 2)
+
+	value, ok := r.Get("foo")
+	if(ok) {
+		fmt.Println(value)
+	}
+	// fmt.Printf("%s", r.Get("foo"))
+}
+
+func InitSeed() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func GenerateTestString(size int64) string {
+	b := make([]byte, size)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func TestInsert(t *testing.T) {
+	// r := New()
+	InitSeed()
+	test_str := GenerateTestString(16)
+	fmt.Println(test_str)
+}
