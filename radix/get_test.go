09ff3085aa85c624fff1d54bf6f23d4da8d51828
SokolovVadim <varem99@mail.ru>
2021-02-21 16:59:28 +0300

    get fixed: str, val

--- radix/get_test.go
+++ radix/get_test.go
@@ -1,33 +1,46 @@
 package radix_test
 
 import (
+	"fmt"
 	"github.com/SokolovVadim/Radix-Tree"
+	"reflect"
 	"testing"
 )
 
 const (
-	Length = 65536;
-	LeftPos = 1024;
-	RightPos = 1032;
+	Length = 30
+	LeftPos = 10
+	RightPos = 20
 )
 
-func GetSubstring(b *testing.B, r *radix.Tree, testStr string) {
-	_, ok := r.Get(testStr[LeftPos: RightPos])
-	if !ok {
-		b.Fatalf("missing key: %v", testStr[LeftPos:RightPos])
+func GetSubstring(b *testing.B, r *radix.Tree, subString string,
+	              testStr string) (str string, val interface{}) {
+	var out string
+	var pos interface{}
+	// there is only one possible match for a string
+	fn := func(s string, v interface{}) bool {
+		out, pos = s, v
+		return false
 	}
+
+	r.WalkPrefix(subString, fn)
+	// fmt.Println("out:", out, "pos =", pos)
+	if !reflect.DeepEqual(out, testStr[LeftPos: Length]) {
+		b.Fatalf("mis-match: %v %v", out, subString)
+	}
+	return out, pos
 }
 
 func BenchmarkGet(b *testing.B) {
 	InitSeed()
 	testStr := GenerateTestString(Length)
-	// println(testStr)
+	fmt.Println(testStr)
 	r := radix.New()
 	var substringArray = CreateSubstrings(testStr)
 	FillRadixTree(Length, r, substringArray)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		GetSubstring(b, r, testStr)
+		GetSubstring(b, r, testStr[LeftPos: RightPos], testStr)
 	}
 }
