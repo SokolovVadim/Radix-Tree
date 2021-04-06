ec083ae8ba7e0a850867bc4ff85fcb3517add2ea
Vadim <varem99@mail.ru>
2021-04-06 10:59:37 +0300

    ibwt

--- bwt/bwt_test.go
+++ bwt/bwt_test.go
@@ -1,10 +1,12 @@
 package bwt
 
 import (
+	"sort"
+	"strings"
 	"testing"
 )
 
-func NaiveBWT(sa* suffixarrayx, text string) []byte {
+func naiveBWT(sa* suffixarrayx, text string) []byte {
 	textLen := len(text)
 	btwArr := make([]byte, textLen)
 	for i := 0; i < textLen; i++ {
@@ -17,13 +19,36 @@ func NaiveBWT(sa* suffixarrayx, text string) []byte {
 	return btwArr
 }
 
+func regenerateSuffix(bwtArr string) string {
+	// make an empty table
+	bwtArr += "^"
+	var table = make([]string, len(bwtArr))
+	for range table {
+		for i := 0; i < len(bwtArr); i++ {
+			table[i] = bwtArr[i:i+1] + table[i]
+		}
+		sort.Strings(table)
+	}
+	for _, row := range table {
+		if strings.HasSuffix(row, "$") {
+			return row[1 : len(bwtArr) - 1]
+		}
+	}
+	return ""
+}
+
 func BenchmarkBWT(b *testing.B) {
 	var input = "SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES$"
 	sa := NewSuffixArrayX(input)
 
-	btwArr := NaiveBWT(sa, input)
-	println(string(btwArr))
-	if string(btwArr) != "STEXYDST.E.IXXIIXXSSMPPS.B..EE.$.USFXDIIOIIIT" {
-		b.Errorf("NaiveBWT is wrong, %s", string(btwArr))
+	bwtArr := naiveBWT(sa, input)
+	println(string(bwtArr))
+	if string(bwtArr) != "STEXYDST.E.IXXIIXXSSMPPS.B..EE.$.USFXDIIOIIIT" {
+		b.Errorf("NaiveBWT is wrong, %s", string(bwtArr))
+	}
+	ibwtArr := regenerateSuffix(string(bwtArr))
+	println("ibwt: ", string(ibwtArr))
+	if string(ibwtArr) != "SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES" {
+		b.Errorf("iBWT is wrong, %s", string(bwtArr))
 	}
 }
