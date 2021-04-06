9c5f8b7dc4c8d343f6056c01f94136e64638d7fa
Vadim <varem99@mail.ru>
2021-04-06 11:36:03 +0300

    regenerate Suffix

--- bwt/bwt_test.go
+++ bwt/bwt_test.go
@@ -1,6 +1,7 @@
 package bwt
 
 import (
+	"fmt"
 	"sort"
 	"strings"
 	"testing"
@@ -19,7 +20,7 @@ func naiveBWT(sa* suffixarrayx, text string) []byte {
 	return btwArr
 }
 
-func regenerateSuffix(bwtArr string) string {
+func inverseBWT(bwtArr string) string {
 	// make an empty table
 	bwtArr += "^"
 	var table = make([]string, len(bwtArr))
@@ -37,18 +38,51 @@ func regenerateSuffix(bwtArr string) string {
 	return ""
 }
 
+func decode(x int, bwtArr []byte) int {
+	hopsToEnd := 0
+	for {
+		if bwtArr[x] == '$'{
+			break
+		}
+		hopsToEnd++
+		x = int(bwtArr[x])
+	}
+	return len(bwtArr) - 1 - hopsToEnd
+}
+
+func regenerateSuffix(bwtArr []byte) []byte {
+	regArr := make([]byte, len(bwtArr))
+	for i := 0; i < len(bwtArr); i++ {
+		regArr[i] = byte(decode(i, bwtArr))
+	}
+	return regArr
+}
+
 func BenchmarkBWT(b *testing.B) {
 	var input = "SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES$"
 	sa := NewSuffixArrayX(input)
+	println("Sa.index:")
+	for i := 0; i < len(sa.index); i++ {
+		fmt.Printf("%d ", sa.index[i])
+	}
 
 	bwtArr := naiveBWT(sa, input)
-	println(string(bwtArr))
+	println("\nBWT: ", string(bwtArr))
+	for i := 0; i < len(bwtArr); i++ {
+		fmt.Printf("%d ", bwtArr[i])
+	}
 	if string(bwtArr) != "STEXYDST.E.IXXIIXXSSMPPS.B..EE.$.USFXDIIOIIIT" {
 		b.Errorf("NaiveBWT is wrong, %s", string(bwtArr))
 	}
-	ibwtArr := regenerateSuffix(string(bwtArr))
-	println("ibwt: ", string(ibwtArr))
+	ibwtArr := inverseBWT(string(bwtArr))
+	println("\niBWT: ", string(ibwtArr))
+	for i := 0; i < len(ibwtArr); i++ {
+		fmt.Printf("%d ", ibwtArr[i])
+	}
 	if string(ibwtArr) != "SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES" {
 		b.Errorf("iBWT is wrong, %s", string(bwtArr))
 	}
+
+	regArr := regenerateSuffix(bwtArr)
+	println("regenerated suffix: ", string(regArr))
 }
