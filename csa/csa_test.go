8bde138f56f281f78fbbade2801a14da70368105
SokolovVadim <varem99@mail.ru>
2021-04-27 23:05:41 +0300

    select

--- csa/csa_test.go
+++ csa/csa_test.go
@@ -3,8 +3,22 @@ import (
 	"testing"
 )
 
-func select_(bv []byte, k int) {
-
+func select_(sa Csa, k int, c int) int {
+	if k <= 0 {
+		return err
+	}
+	idx := sa.sa[c]
+	if idx == 0 && c != 0 {
+		return sa.len
+	}
+	if idx == 255 {
+		return err
+	}
+	if int(sa.sa[idx]) + k - 1 < int(sa.sa[idx + 1]) {
+		return int(sa.psi[int(sa.sa[idx]) + k - 1])
+	} else {
+		return sa.len
+	}
 }
 
 func rank(bv []byte, x int) {
