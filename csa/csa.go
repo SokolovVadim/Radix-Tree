19e7649596183e29a6b30748e8cd869a20198be7
Vadim <varem99@mail.ru>
2021-05-27 23:41:47 +0300

    search preparation

--- csa/csa.go
+++ csa/csa.go
@@ -7,21 +7,21 @@ import (
 const (
 	indexNotFound = -1
 	alphabetLength = 2
+	EofMarker = 1000
 )
 
 type Csa struct {
 	suffixOffsets   []int
 	psi  []uint32
-	bv   []int
 	ef   *CompressedText
-	len int
+	length int
 }
 
 func newCsa(text string) *Csa {
 	sa := NewSuffixArrayX(text)
 	csa := new(Csa)
 	csa.suffixOffsets = sa.index
-	csa.len = sa.n
+	csa.length = sa.n
 	csa.psi = csa.naivePsi()
 	return csa
 }
@@ -29,7 +29,7 @@ func newCsa(text string) *Csa {
 func (csa *Csa)printContents() {
 	println("========== Printing contents ==========")
 	println("Offset:")
-	for i := 0; i < csa.len; i++ {
+	for i := 0; i < csa.length; i++ {
 		fmt.Printf("%v ", i)
 	}
 	println("\nSuffix array:")
@@ -38,10 +38,6 @@ func (csa *Csa)printContents() {
 	}
 	println("\nPsi array:")
 	for _, i := range csa.psi {
-		fmt.Printf("%v ", i)
-	}
-	println("\nBit vector:")
-	for _, i := range csa.bv {
 		fmt.Printf("%v ", i)
 	}
 	println("\nBitmap:")
@@ -59,32 +55,38 @@ func findIndex(saIndex []int, idx int) int {
 }
 
 func (csa* Csa)naivePsi() []uint32 {
-	psiArr := make([]uint32, csa.len)
+	psiArr := make([]uint32, csa.length)
 	// assume PSI[0] = '$'
-	psiArr[0] = 0
-	for i := 1; i < csa.len; i++ {
+	// PSI[0] = index, where SA[index] = 0
+	psiArr[0] = EofMarker
+	for i := 1; i < csa.length; i++ {
 		psiArr[i] = uint32(findIndex(csa.suffixOffsets, csa.suffixOffsets[i] + 1))
 	}
 	csa.psi = psiArr
 	return psiArr
 }
 
-func (csa* Csa) createBitVector() {
-	// length = N + #items ~= length * 2
-	length := csa.len * 2
-	bv := make([]int, length)
-	for i := 0; i < length; i++ {
-		bv[i] = 0
-	}
-	for i := 0; i < csa.len; i++ {
-		// fmt.Println("i = ", i, ", psi[i] = ", csa.psi[i], ", i + psi[i] = ", i + int(csa.psi[i]))
-		bv[i + int(csa.psi[i])] = 1
-	}
-	csa.bv = bv
-}
-
 func (csa* Csa)efCompress() {
 	// create an Elias-Fano sequence with maximum element from psi
 	csa.ef = NewEF(uint64(len(csa.psi)), uint64(len(csa.psi)))
-	csa.ef.Compress(csa.psi[:len(csa.psi) / alphabetLength])
+	csa.ef.Compress(csa.psi[(len(csa.psi) / alphabetLength):])
+}
+
+func (csa* Csa)getSaFromPsi(x int, psi []uint32) int{
+	// General relationship:
+	// SA[x] = SA[PSI[X]] - 1
+	// while(psi[x] != EOF_MARKER)
+	hopsToEnd := 0
+	for {
+		if psi[x] == psi[0] {
+			break
+		}
+		hopsToEnd++
+		x = int(psi[x])
+	}
+	return csa.length - hopsToEnd - 1
+}
+
+func (csa* Csa)Lookup(str string) {
+
 }
--- csa/csa_test.go
+++ csa/csa_test.go
@@ -1,6 +1,7 @@
 package csa
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -18,9 +19,9 @@ func BenchmarkCSA(b *testing.B) {
 func TestCsa(t *testing.T) {
 	input := "abbaabbaaababbb$"
 	suffixArray := []int{15, 7, 8, 3, 9, 4, 0, 11, 14, 6, 2, 10, 13, 5, 1, 12}
-	psiArray    := []uint32{0, 2, 4, 5, 11, 13, 14, 15, 0, 1, 3, 7, 8, 9, 10, 12}
-	bitVector   := []int{1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0}
-	bitString := "{1,4,7,9,16,19,21,23}"
+	psiArray    := []uint32{EofMarker, 2, 4, 5, 11, 13, 14, 15, 0, 1, 3, 7, 8, 9, 10, 12}
+
+	// bitString := "{1,4,7,9,16,19,21,23}"
 	csa := newCsa(input)
 	if !reflect.DeepEqual(suffixArray, csa.suffixOffsets) {
 		t.Errorf("suffix array is wrong")
@@ -28,16 +29,15 @@ func TestCsa(t *testing.T) {
 	if !reflect.DeepEqual(psiArray, csa.psi) {
 		t.Errorf("psi-array is wrong")
 	}
-	csa.createBitVector()
-	if !reflect.DeepEqual(bitVector, csa.bv) {
-		t.Errorf("bit vector is wrong")
-	}
-	csa.efCompress()
-	if bitString != csa.ef.b.String() {
+	// csa.efCompress()
+/*	if bitString != csa.ef.b.String() {
 		t.Errorf("bit map is wrong")
-	}
-	if !reflect.DeepEqual(csa.ef.getMany(len(csa.psi) / alphabetLength), psiArray[:len(csa.psi) / alphabetLength]) {
-		t.Errorf("bit map is wrong")
-	}
-	csa.printContents()
+	}*/
+/*	if !reflect.DeepEqual(csa.ef.getMany(len(csa.psi) / alphabetLength), psiArray[:len(csa.psi) / alphabetLength]) {
+		t.Errorf("converted psi-array is wrong")
+	}*/
+	fmt.Println(csa.psi)
+	x := csa.getSaFromPsi(13, csa.psi)
+	fmt.Println(x)
+	// csa.printContents()
 }
