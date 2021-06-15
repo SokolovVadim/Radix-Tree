7ac40f0b3aa711b91cc4234f09150147ee9b845f
Vadim <varem99@mail.ru>
2021-06-15 09:58:17 +0300

    marker added

--- plot_builder/mem_plotter.py
+++ plot_builder/mem_plotter.py
@@ -7,15 +7,15 @@ sa_len = [879, 782, 684, 586, 498, 391, 293, 196, 98, 49, 10, 5, 2]
 sa_mem = [5279, 4695, 4111, 3527, 2949, 2349, 1765, 1183, 599, 309, 60, 29, 9]
 
 fig, ax = plt.subplots()
-fig.suptitle('Lookup substring', fontsize=14)
+fig.suptitle('Memory usage', fontsize=14)
 
 plt.xlabel('Document size, Kbyte', fontsize=12)
 plt.ylabel('Heap allocation, kByte', fontsize=12)
 
-ax.plot(csa_len, csa_mem, c = 'r', label = 'CSA')
-ax.plot(sa_len, sa_mem, c = 'b', label = 'SA')
+ax.plot(csa_len, csa_mem, marker = 'o', c = 'r', label = 'CSA')
+ax.plot(sa_len, sa_mem, marker = 'o', c = 'b', label = 'SA')
 
 ax.legend(loc ="upper left")
-fig.savefig('csa_sa.jpg')
+fig.savefig('csa_sa_marker_o.jpg')
 
 plt.show()
